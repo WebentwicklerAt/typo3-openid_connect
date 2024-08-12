f4c0dfb4e6cde629d84e48e907cf4a158f81e65f
Gernot Leitgab <gernot@leitgab.at>
2024-08-12 08:28:20 +0200

    Update TYPO3 version requirement to 12

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'plugin',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.4.99',
+            'typo3' => '12.4.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
